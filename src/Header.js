84d249e748035ec30dd82679f7157b98e31672cc
lauradevuono <lauradevuono@gmail.com>
2023-01-09 17:17:07 -0500

    settng up skeleton, importing fonts

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="indexHeader">
+            <h1 className="wrapper">Welcome to... <span>Horror Roulette</span></h1>
+            {/* <a href="roulette.html">Enter if you dare</a> */}
+        </header>
+    );
+}
+
+export default Header;
