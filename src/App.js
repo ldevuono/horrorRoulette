200e235c699dafecce9fd5a543f18f2d891b0376
lauradevuono <lauradevuono@gmail.com>
2023-01-10 14:36:17 -0500

    building out components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './Header.js';
+import Roulette from './Roulette.js';
+import Form from './Form.js';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Roulette />
+      <Form />
     </div>
   );
 }
---
+++ src/Roulette.js
@@ -0,0 +1,9 @@
+function Roulette() {
+    return (
+        <header className="indexHeader">
+            <h1 className="wrapper">It's time to decide</h1>
+        </header>
+    );
+}
+
+export default Roulette;
