b37c6d6d16f477656a85ab0ee69b784fd98c56b0
lauradevuono <lauradevuono@gmail.com>
2023-01-16 14:36:48 -0500

    ternary in movieBox

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,10 +1,10 @@
 function MovieBox(props) {
     return (
         <div className="chooseMovie">
-            <h2>{props.title} </h2>
-            <p>{props.desc}</p>
-            <img src={`https://image.tmdb.org/t/p/w500${props.path}`}
-                alt={props.title} />
+            <h2>{props.randomMovie.title} </h2>
+            <p>{props.randomMovie.overview}</p>
+            {props.randomMovie.hasOwnProperty('poster_path') ? <img src={`https://image.tmdb.org/t/p/w500${props.randomMovie.poster_path}`}
+                alt={`poster for ${props.title}`} /> : null}
         </div>
     );
 }
