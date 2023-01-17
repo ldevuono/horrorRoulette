fc8dbfb80eec9e850a17168ab441852cdeeb6a89
lauradevuono <lauradevuono@gmail.com>
2023-01-17 11:37:33 -0500

    Watchlist styling

--- src/WatchList.js
+++ src/WatchList.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 function WatchList(props) {
     return (
-        <div>
+        <div className="watchListBox">
             {props.savedMovie.map((movie) => {
                 return (
-                    <div className="chooseMovie" key={movie.key}>
-                        <h2>{movie.title}</h2>
-                        <p>{movie.overview}</p>
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    <ul key={movie.key}>
+                        <li><h2>{movie.title}</h2></li>
+                        <li><p>{movie.overview}</p></li>
+                        <li className="img"><img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                             alt={`poster for ${movie.title}`}
-                        />
-                    </div>
+                        /></li>
+                    </ul>
                 )
             })
             }
