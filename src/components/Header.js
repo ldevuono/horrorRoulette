d571f3c5013f1759a0cc7b94693e29147a40f7e4
lauradevuono <lauradevuono@gmail.com>
2023-02-05 18:21:02 -0500

    styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
-// import background from "./../vhs.png"
+import die from "./../assets/dice.png"
 
 function Header() {
     return (
         <header className="indexHeader">
             <h1 className="wrapper">Welcome to... <span>Horror Roulette</span></h1>
             <Link to="/roulette"> <p className="button">Enter if you dare</p></Link>
+            <img className="dice" src={die} alt="die" />
         </header >
     );
 }
--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 
-
 function WatchList(props) {
     return (
         <div>
