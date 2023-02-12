91ed543ec66fe58b16c98da60fe4e7836cbe7e73
lauradevuono <lauradevuono@gmail.com>
2023-02-11 22:43:44 -0500

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import { Link } from 'react-router-dom';
-import die from "./../assets/dice.png"
 
 function Header() {
     return (
         <header className="indexHeader">
             <h1 className="wrapper">Welcome to... <span>Horror Roulette</span></h1>
             <Link to="/roulette"> <p className="button">Enter if you dare</p></Link>
-            <img className="dice" src={die} alt="die" />
         </header >
     );
 }
--- src/components/Roulette.js
+++ src/components/Roulette.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import Form from './Form.js';
 import MovieBox from './MovieBox.js';
 
+
 function Roulette(props) {
 
     return (
